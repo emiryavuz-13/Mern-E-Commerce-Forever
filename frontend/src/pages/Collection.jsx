9acf8cd40e84d1ae998e3da2d2525cc172f21a6c
EmirYavuz-Pc <yvz.emir13@gmail.com>
2025-03-25 05:49:37 +0300

    Collection sayfası yapılmaya başlandı.

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Collection = () => {
-  return <div>Collection</div>;
+  const { products } = useContext(ShopContext);
+  const [shotFilter, setShortFilter] = useState(false);
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* Filter Options*/}
+      <div className="min-w-60">
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+          FILTERS
+        </p>
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            shotFilter ? "" : "hidden"
+          } sm:block`}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Collection;
