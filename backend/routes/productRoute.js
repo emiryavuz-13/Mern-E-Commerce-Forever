4c78f28f140a38521a8eff740f75998282822b69
Emir Yavuz - Lenova <yvz.emir13@gmail.com>
2025-04-03 22:15:21 +0300

    productRoute, productController, multer dosyaları oluşturuldu.

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  addProduct,
+  singleProduct,
+  removeProduct,
+  listProduct,
+} from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
+
+const productRouter = express.Router();
+productRouter.post(
+  "/add",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
+productRouter.post("/single", singleProduct);
+productRouter.post("/remove", removeProduct);
+productRouter.post("/list", listProduct);
+
+export default productRouter;
