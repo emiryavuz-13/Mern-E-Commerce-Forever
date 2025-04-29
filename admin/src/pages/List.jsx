4cc53d4be011c44e1f21195ffae916095ffd175e
EmirYavuz-Pc <yvz.emir13@gmail.com>
2025-04-30 02:52:31 +0300

    Admin List sayfası yapılmaya başlandı.

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
 const List = ({ token }) => {
-  return <div></div>;
+  const [List, setList] = useState([]);
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+      if (response.data.success) {
+        setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchList();
+  }, []);
+  return (
+    <>
+      <p className="mb-2">All Products List</p>
+      <div className="flex flex-col gap-2">
+        {/* List Table Title */}
+        <div className="hidden md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default List;
